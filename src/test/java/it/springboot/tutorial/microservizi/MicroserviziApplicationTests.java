1a468ec7073f34c244a70648adf014034650b951
Edoardo Baral <edoardo@baral.it>
2021-05-20 21:17:51 +0200

    Avvio del progetto

---
+++ src/test/java/it/springboot/tutorial/microservizi/MicroserviziApplicationTests.java
@@ -0,0 +1,13 @@
+package it.springboot.tutorial.microservizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
